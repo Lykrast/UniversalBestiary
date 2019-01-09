75ee74ff7647e83fb75aee02d67a1aa50816378b
Lykrast <aymericpm@gmail.com>
2019-01-09 14:10:19 +0100

    Initial setup stuff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# UniversalBestiary
-Minecraft mod that adds a bestiary for infos on mobs you encounter
+# Universal Bestiary
+
+Minecraft mod that adds a bestiary for infos on mobs you encounter, including modded mobs. Scan them with the Identifier to unlock their entry.
---
+++ src/main/java/lykrast/universalbestiary/UniversalBestiary.java
@@ -0,0 +1,33 @@
+package lykrast.universalbestiary;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = UniversalBestiary.MODID, 
+	name = UniversalBestiary.NAME, 
+	version = UniversalBestiary.VERSION, 
+	acceptedMinecraftVersions = "[1.12, 1.13)")
+public class UniversalBestiary {
+    public static final String MODID = "universalbestiary";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "@VERSION@";
+
+	public static Logger logger = LogManager.getLogger(MODID);
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+		
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
